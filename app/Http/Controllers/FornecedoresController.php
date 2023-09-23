f53acb137828fd36617192d895d2dd16edbe6157
joao teodoro <joaotdn@gmail.com>
2023-09-23 04:33:10 -0300

    comandos blade

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -6,8 +6,31 @@ use Illuminate\Http\Request;
 
 class FornecedoresController extends Controller
 {
-    public function index() {
-        $fornecedores = [];
+    public function index()
+    {
+        $fornecedores = [
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '123',
+                'ddd' => '11',
+                'telefone' => '000-0000'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85',
+                'telefone' => '000-0000'
+            ],
+            2 => [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32',
+                'telefone' => '000-0000'
+            ],
+        ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -6,10 +6,52 @@
 
 {{-- @dd($fornecedores) --}}
 
-@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+{{-- @if (count($fornecedores) > 0 && count($fornecedores) < 10)
     <h3>Existem alguns fornecedores</h3>
 @elseif(count($fornecedores) > 10)
     <h3>Existem muitos fornecedores</h3>
 @else 
     <h3>Ainda não exitem fornecedores</h3>
-@endif
+@endif --}}
+{{-- 
+@unless ($fornecedores[0]['status'] == 'S')
+    Fornecedor Inativo
+@endunless --}}
+
+{{-- @isset($fornecedores)
+    @isset($fornecedores[1])
+        Exite 2 fornecedores
+    @endisset
+@endisset --}}
+
+{{-- @empty($fornecedores)
+    <p>Não existe fornecedores</p>
+@endempty --}}
+
+{{-- @isset($fornecedores)
+    @forelse ($fornecedores as $fornecedor)
+    Iteração atual {{ $loop->iteration }}
+    CNPJ:  {{ $fornecedor['cnpj'] ?? 'Não preenchido'  }}
+    <br>
+    Cidade: {{ $fornecedor['ddd'] ?? 'Não preenchido' }}
+    @switch($fornecedor['ddd'])
+        @case('11')
+            São Paulo
+            @break
+        @case('32')
+            Juiz de Fora
+            @break
+        @case('85')
+            Fortaleza
+            @break
+        @default
+            Não exite
+    @endswitch
+    @if ($loop->last)
+        Última iteração
+    @endif
+    <hr>
+    @empty
+        <p>Não exite fornecedor</p>
+    @endforelse
+@endisset --}}
