9253caccd3e063e76e0bd7de9ad8447a7d114145
joao teodoro <joaotdn@gmail.com>
2023-10-07 14:07:34 -0300

    validating and show errors from contact form

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -16,13 +16,24 @@ class ContatoController extends Controller
 
     public function salvar(Request $request)
     {
-        $request->validate([
-            'nome' => 'required|min:3|max:40',
-            'telefone' => 'required',
-            'email' => 'email|unique:site_contatos',
-            'motivo_contatos_id' => 'required',
-            'mensagem' => 'required|min:10|max:200',
-        ]);
+        $request->validate(
+            [
+                'nome' => 'required|min:3|max:40',
+                'telefone' => 'required',
+                'email' => 'email|unique:site_contatos',
+                'motivo_contatos_id' => 'required',
+                'mensagem' => 'required|min:10|max:200',
+            ],
+            [
+                'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+                'email.email' => 'O campo email deve conter um email válido',
+                'email.unique' => 'O campo email já foi utilizado',
+                'mensagem.min' => 'O campo mensagem deve ter no mínimo 10 caracteres',
+                'mensagem.max' => 'O campo mensagem deve ter no máximo 200 caracteres',
+                'required' => 'O campo :attribute deve ser preenchido'
+            ]
+        );
         SiteContato::create($request->all());
         return redirect()->route('site.index');
     }
